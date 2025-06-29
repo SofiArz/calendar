d32cdba2d27db1a8f193e9ba0e9873593fff4dad
Sofia Arzamendia <sofia.arzamendia@hotmail.es>
2025-06-29 22:37:19 +0200

    serializableCheck

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,9 @@ export const store = configureStore({
     reducer: {
         calendar: calendarSlice.reducer,
         ui: uiSlice.reducer,
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    })
 
 })
